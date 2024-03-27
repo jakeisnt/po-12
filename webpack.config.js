1d7ea9fb7cc9ff77643fb97ab6a1ce308153086b
Jacob Chvatal <jake@isnt.online>
2024-03-27 20:08:06 +0100

    wt

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,44 @@
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                localIdentName: "[name]__[local]___[hash:base64:5]",
-              },
-              sourceMap: true,
-            },
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-typescript"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                {
+                  runtime: "automatic",
+                  importSource: "react",
+                },
+              ],
+              [
+                "react-css-modules",
+                {
+                  generateScopedName: "[name]__[local]___[hash:base64:5]",
+                  webpackHotModuleReloading: true,
+                  filetypes: {
+                    ".scss": {
+                      syntax: "postcss-scss",
+                    },
+                  },
+                },
+              ],
+            ],
           },
-          "sass-loader",
-        ],
+        },
       },
+      // ... other rules ...
     ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".scss", ".svg"],
   },
 };
