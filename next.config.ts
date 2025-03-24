f2f9d863dfe3418772e2e5ad8872aaeabb26a796
Jacob Chvatal <jake@isnt.online>
2025-03-24 11:24:39 +0100

    remove shader support, upgrade bun, bump packages, improve package.json

--- next.config.ts
+++ next.config.ts
@@ -32,12 +32,6 @@ const nextConfig = {
       type: "asset/resource",
     });
 
-    // Shader support
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      use: ["raw-loader", "glslify-loader"],
-    });
-
     return config;
   },
 };
